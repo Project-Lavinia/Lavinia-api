2be30d79bb0c9df1a40dbba6c4c1be8be2657bce
Anders Jakob Sivesind <ajsivesind@gmail.com>
2019-01-31 16:48:09 +0100

    Made election private and read only. https://github.com/Project-Lavinia/Lavinia-api/pull/11#discussion_r227163523

--- Lavinia-api/Tests/ModelBuilderTests.cs
+++ Lavinia-api/Tests/ModelBuilderTests.cs
@@ -10,13 +10,13 @@ namespace LaviniaApi.Tests
 {
     public class ModelBuilderTests
     {
-        ElectionFormat election = new ElectionFormat().Parse("2013;Sainte Laguës (modified);1.4;4.0;1.8;150;19",
+        private readonly ElectionFormat _election = new ElectionFormat().Parse("2013;Sainte Laguës (modified);1.4;4.0;1.8;150;19",
             new FieldParser("TEST", ";"));
 
         [Fact]
         public void BuildAlgorithmParametersTest()
         {
-            AlgorithmParameters ap = ModelBuilder.BuildAlgorithmParameters(election);
+            AlgorithmParameters ap = ModelBuilder.BuildAlgorithmParameters(_election);
             Assert.Equal(AlgorithmUtilities.ModifiedSainteLagues, ap.Algorithm);
             //Assert.Equal(1.4, ap.Parameters["FirstDivisor"]);
         }
